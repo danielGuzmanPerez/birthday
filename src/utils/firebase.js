af252ba88326a8d57d0412a87e8f73a115844f3d
Victor Daniel Guzman Perez <vdgp_12@hotmail.com>
2020-12-21 20:20:44 -0600

    ya funciona el logeo

---
+++ src/utils/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app'
+const firebaseConfig = {
+    apiKey: "AIzaSyDxy70AvnNlGlXJAf8bCHASFr0f-YsmAWY",
+    authDomain: "birthday-32e40.firebaseapp.com",
+    projectId: "birthday-32e40",
+    storageBucket: "birthday-32e40.appspot.com",
+    messagingSenderId: "978396180246",
+    appId: "1:978396180246:web:3df7d454c585689fd7ea37"
+  };
+ export default firebase.initializeApp(firebaseConfig);
